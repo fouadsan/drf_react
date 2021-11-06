b8788a81bfd715e8bfaafe338573cebcba665c1b
fouadsan <F.benayad95@gmail.com>
2021-11-06 19:06:00 +0100

    Added image upload to the api

--- drf/blog_api/views.py
+++ drf/blog_api/views.py
@@ -11,11 +11,16 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import filters
 
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+
 
 class PostList(generics.ListAPIView):
 
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+
 
 class PostDetail(generics.RetrieveAPIView):
 
@@ -25,7 +30,7 @@ class PostDetail(generics.RetrieveAPIView):
         item = self.kwargs.get('pk')
         return get_object_or_404(Post, slug=item)
 
-    
+
 # Post Search
 class PostListDetailFilter(generics.ListAPIView):
     queryset = Post.objects.all()
@@ -33,31 +38,47 @@ class PostListDetailFilter(generics.ListAPIView):
     filter_backends = [filters.SearchFilter]
     search_fields = ['^slug']
 
-     # '^' Starts-with search.
+    # '^' Starts-with search.
     # '=' Exact matches.
     # '@' Full-text search. (Currently only supported Django's PostgreSQL backend.)
     # '$' Regex search.
 
 # Post Admin
-class CreatePost(generics.CreateAPIView):
+
+
+# class CreatePost(generics.CreateAPIView):
+#     permission_classes = [permissions.IsAuthenticated]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+class CreatePost(APIView):
     permission_classes = [permissions.IsAuthenticated]
-    queryset = Post.objects.all()
-    serializer = PostSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, format=None):
+        print(request.data)
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AdminPostDestail(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
     queryset = Post.objects.all()
-    serializer = PostSerializer
+    serializer_class = PostSerializer
 
 
 class EditPost(generics.UpdateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     queryset = Post.objects.all()
-    serializer = PostSerializer
+    serializer_class = PostSerializer
 
 
 class DeletePost(generics.RetrieveDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
     queryset = Post.objects.all()
-    serializer = PostSerializer
+    serializer_class = PostSerializer
